06af3520ccdd0c08e4fa5f37397f45b3ddc485f6
user <user@HONGYJ>
2021-04-28 21:45:47 +0900

    1

--- src/main/java/com/sist/controller/MypageController.java
+++ src/main/java/com/sist/controller/MypageController.java
@@ -13,7 +13,8 @@ public class MypageController {
 	
 	
 	
-	//메인으로 전송 //관심목록(찜&장바구니)
+	//메인으로 전송
+	//관심목록(찜&장바구니)
 	@GetMapping("mypage/like_list.do")
 	public String mypage_mypage(Model model){
 		model.addAttribute("mypage_jsp","../mypage/like_list.jsp");
@@ -21,18 +22,10 @@ public class MypageController {
 		return "main/main";
 	}
 	
-	
-	@GetMapping("mypage/like_list.do")
-		public String mypage_like_list(Model model){
-		
-		model.addAttribute("mypage_jsp","../mypage/like_list.jsp");
-		model.addAttribute("main_jsp","../mypage/mypage_main.jsp");
-		return "main/main";
-	}
 	//구매정보
 	@GetMapping("mypage/buy_list.do")
 	public String mypage_buy_list(Model model){
-		model.addAttribute("mypage_jsp","../mypage/like_list.jsp");
+		model.addAttribute("mypage_jsp","../mypage/buy_list.jsp");
 		model.addAttribute("main_jsp","../mypage/mypage_main.jsp");
 		return "main/main";
 	}
@@ -40,7 +33,7 @@ public class MypageController {
 	//나만의 계획(채팅정보 불러오기)
 	@GetMapping("mypage/schedule.do")
 	public String mypage_schdule(Model model){
-		model.addAttribute("mypage_jsp","../mypage/like_list.jsp");
+		model.addAttribute("mypage_jsp","../mypage/schedule.jsp");
 		model.addAttribute("main_jsp","../mypage/mypage_main.jsp");
 		return "main/main";
 	}
@@ -48,7 +41,7 @@ public class MypageController {
 	@GetMapping("mypage/mypage_update.do")
 	public String mypage_update(Model model){
 		
-		model.addAttribute("mypage_jsp","../mypage/like_list.jsp");
+		model.addAttribute("mypage_jsp","../mypage/mypage_update.jsp");
 		model.addAttribute("main_jsp","../mypage/mypage_main.jsp");
 		return "main/main";
 	}
