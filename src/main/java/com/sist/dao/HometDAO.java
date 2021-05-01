74719553a8acb6b7fc404628888e076861587f66
JunHo <u_innovation@naver.com>
2021-05-01 16:21:09 +0900

    config, connection

--- src/main/java/com/sist/dao/HometDAO.java
+++ src/main/java/com/sist/dao/HometDAO.java
@@ -1,5 +1,23 @@
 package com.sist.dao;
 
-public class HometDAO {
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import com.sist.vo.HometMainVO;
+
+import java.util.*;
+@Repository
+public class HometDAO extends SqlSessionDaoSupport{
+
+	@Autowired
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		// TODO Auto-generated method stub
+		super.setSqlSessionFactory(sqlSessionFactory);
+	}
+	// 목록 출력
+	public List<HometMainVO> hometListData(Map map) {
+		return getSqlSession().selectList("hometListData",map);
+	}
 }
