cfd494da0d5ab236943c29377c2102b731b745ec
JunHo <u_innovation@naver.com>
2021-05-13 02:09:31 +0900

    fix::error

---
+++ src/main/java/com/sist/dao/HometBoardDAO.java
@@ -0,0 +1,126 @@
+package com.sist.dao;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sist.vo.FreeBoardVO;
+
+import java.util.*;
+@Repository
+public class HometBoardDAO extends SqlSessionDaoSupport{
+
+	@Autowired
+	public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+		// TODO Auto-generated method stub
+		super.setSqlSessionFactory(sqlSessionFactory);
+	}
+	/*
+	 *  <select id="freeboardListData" resultType="FreeBoardVO" parameterType="hashmap">
+		 SELECT no,subject,name,regdate,hit,num
+		 FROM (SELECT no,subject,name,regdate,hit,rownum as num 
+		 FROM (SELECT /*+ INDEX_DESC(freeboard fb_no_pk) /no,subject,name,regdate,hit
+		 FROM freeboard))
+		 WHERE num BETWEEN #{start} AND #{end}
+		</select>
+		<select id="freeboardTotalPage" resultType="int">
+		 SELECT CEIL(COUNT(*)/10.0) FROM freeboard
+		</select>
+	 */
+	public List<FreeBoardVO> freeboardListData(Map map)
+	{
+		return getSqlSession().selectList("freeboardListData", map);
+	}
+	public int freeboardTotalPage()
+	{
+		return getSqlSession().selectOne("freeboardTotalPage");
+	}
+	// 글쓰기
+	public void freeboardInsert(FreeBoardVO vo)
+	{
+		getSqlSession().insert("freeboardInsert", vo);
+	}
+	// 상세보기
+		public FreeBoardVO freeboardDetailData(int no)
+		{
+			getSqlSession().update("freeboardHitIncrement", no);
+			return getSqlSession().selectOne("freeboardDetailData", no);
+		}
+		public FreeBoardVO freeboardUpdateData(int no)
+		{
+			return getSqlSession().selectOne("freeboardDetailData", no);
+		}
+		
+		
+		// 수정
+		public boolean freeboardUpdate(FreeBoardVO vo)
+		{
+			boolean bCheck=false;
+			// 비밀번호를 가지고 온다
+			String db_pwd=getSqlSession().selectOne("freeboardGetPassword", vo.getNo());
+			if(db_pwd.equals(vo.getPwd()))
+			{
+				bCheck=true;
+				getSqlSession().update("freeboardUpdate", vo);
+			}
+			else
+			{
+				bCheck=false;
+			}
+			return bCheck;
+		}
+
+		@Transactional(propagation=Propagation.REQUIRED,rollbackFor=Exception.class)
+	     public boolean freeboardDelete(int no,String pwd)
+	     {
+	    	 boolean bCheck=false;
+	    	 // 비밀번호 읽기
+	    	 String db_pwd=getSqlSession().selectOne("freeboardGetPassword",no);
+	    	 System.out.println("db_pwd="+db_pwd+",pwd="+pwd);
+	    	 if(pwd.equals(db_pwd))
+	    	 {
+	    		 bCheck=true;
+	    		 /*
+	    		  *   conn.setAutoCommit(false)
+	    		  */
+	    		 getSqlSession().delete("freeboardReplyDelete",no);
+	    		 getSqlSession().delete("freeboardDelete",no);
+	    		 /*
+	    		  *  conn.commit()  => @Around
+	    		  *  => 오류 발생 => conn.rollback() => @AfterThrowing
+	    		  *  => 정상 수행 => conn.setAutoCommit(true) => @After(finally)
+	    		  */
+	    	 }
+	    	 return bCheck;
+	     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
