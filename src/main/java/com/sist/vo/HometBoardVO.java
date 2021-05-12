3f23c44d953a7cef30c074289c3df9a4f449ed14
JunHo <u_innovation@naver.com>
2021-05-13 01:31:40 +0900

    config추가, 홈트 게시판 작업중

---
+++ src/main/java/com/sist/vo/HometBoardVO.java
@@ -0,0 +1,88 @@
+package com.sist.vo;
+
+import java.util.Date;
+
+public class HometBoardVO {
+	private int no;
+	private String id;
+	private String name;
+	private String subject;
+	private String pwd;
+	private String content;
+	private Date regdate;
+	private int hit;
+	private String dbday;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public String getDbday() {
+		return dbday;
+	}
+
+	public void setDbday(String dbday) {
+		this.dbday = dbday;
+	}
+
+}
